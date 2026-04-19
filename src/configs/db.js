2665bf14f5e72a341709536c0583af0e3f889195
Luiz Fellipe Resende Lima <luizfresende26@gmail.com>
2026-04-19 11:00:14 -0300

    fix: restruturando o local das pastas e correção da variavel DB_USERNAME

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,9 +1,9 @@
-const { underscoredIf } = require("sequelize/lib/utils");
+require("dotenv").config();
 
 module.exports = {
   dialect: process.env.DIALECT,
   host: process.env.HOST,
-  username: process.env.USERNAME,
+  username: process.env.DB_USERNAME,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
   port: process.env.DB_PORT,
