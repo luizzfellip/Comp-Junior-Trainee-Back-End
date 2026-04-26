b16745ed18cfdf243209a129169ac716187ba3f7
Luiz Fellipe Resende Lima <luizfresende26@gmail.com>
2026-04-26 11:18:00 -0300

    feat: inicializa o model Users com a conexao do banco

--- src/database/index.js
+++ src/database/index.js
@@ -1,14 +1,18 @@
 const Sequelize = require("sequelize");
+const Users = require("../apps/modules/Users");
 
+const models = [Users];
 const databaseConfig = require("../configs/db");
 
 class Database {
   constructor() {
-    this.init;
+    this.init();
   }
 
   init() {
     this.connection = new Sequelize(databaseConfig);
+
+    models.map((model) => model.init(this.connection));
   }
 }
 
