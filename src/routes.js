a416f76ff390458918b75dd36e01389d38bfb83f
Luiz Fellipe Resende Lima <luizfresende26@gmail.com>
2026-05-23 19:48:16 -0300

    fix: erro do usuario atualizar o email para um invalido

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,17 @@ const { Router } = require("express");
 const schemaValidator = require("./apps/middlewares/schemaValidator");
 
 const AuthenticationMiddleware = require("./apps/middlewares/authentication");
+const AdminAuthorizeMiddleware = require("./apps/middlewares/adminAuthourize");
 
 const AuthenticationController = require("./apps/controllers/AuthenticadorControler");
 const authSchema = require("./apps/schema/auth.schema.json");
 
 const UserControler = require("./apps/controllers/UserControle");
-const userSchema = require("./apps/schema/create.user.schema.json");
+const createUserSchema = require("./apps/schema/create.user.schema.json");
+const updateUserSchema = require("./apps/schema/update.user.schema.json");
+
+const BookController = require("./apps/controllers/BookController");
+const bookSchema = require("./apps/schema/create.book.schema.json");
 
 const asyncHandler = require("./apps/middlewares/asyncHandler");
 
@@ -19,7 +24,7 @@ routes.get("/", (req, res) => {
 
 routes.post(
   "/user",
-  schemaValidator(userSchema),
+  schemaValidator(createUserSchema),
   asyncHandler(UserControler.createUser),
 );
 
@@ -31,7 +36,30 @@ routes.post(
 
 routes.use(AuthenticationMiddleware);
 
-routes.put("/user", asyncHandler(UserControler.update));
+routes.put(
+  "/user",
+  schemaValidator(updateUserSchema),
+  asyncHandler(UserControler.update),
+);
 routes.delete("/user", asyncHandler(UserControler.delete));
+
+routes.get(
+  "/book",
+  schemaValidator(bookSchema),
+  asyncHandler(BookController.getBooks),
+);
+routes.use(AdminAuthorizeMiddleware("admin"));
+
+routes.put(
+  "/book/:id",
+  schemaValidator(bookSchema),
+  asyncHandler(BookController.updateBook),
+);
+
+routes.post(
+  "/book",
+  schemaValidator(bookSchema),
+  asyncHandler(BookController.createBook),
+);
 
 module.exports = routes;
